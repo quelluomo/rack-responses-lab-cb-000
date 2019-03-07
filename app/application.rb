317f6dae080293dcaf620875b042f78bd5038784
Matteo Muscas <thatman.j@gmail.com>
2019-03-07 09:44:27 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,12 +3,9 @@ class Application
   def call(env)
     resp = Rack::Response.new
     t = Time.now
+    resp.write = t
 
-    if
-      t < 12
-      resp.write "Good Morning!"
-    else
-      resp.write "Good Afternoon!"
+  
     end
     resp.finish
   end
